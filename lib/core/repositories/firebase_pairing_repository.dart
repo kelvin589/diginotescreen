04c5f494c6477635cf40900a9972d49abaf4e6cb
kelvin589 <190081735@aston.ac.uk>
2022-02-13 20:51:56 +0000

    Add screen pairing class
    To make read and write operations type-safe
    Add 'paired' Boolean to track state

--- lib/core/repositories/firebase_pairing_repository.dart
+++ lib/core/repositories/firebase_pairing_repository.dart
@@ -1,18 +1,20 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:diginotescreen/core/models/screen_pairing_model.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
 
 class FirebasePairingRepository {
   final CollectionReference _pairingCodes =
-      FirebaseFirestore.instance.collection('pairingCodes');
+      FirebaseFirestore.instance.collection('pairingCodes').withConverter<ScreenPairing>(
+            fromFirestore: (snapshot, _) => ScreenPairing.fromJson(snapshot.data()!),
+            toFirestore: (screenPairing, _) => screenPairing.toJson(),
+          );
 
   Future<void> addPairingCode(String pairingCode) async {
     String? token = await FirebaseMessaging.instance.getToken();
 
     return _pairingCodes
         .doc(token)
-        .set({
-          'pairingCode': pairingCode,
-        })
+        .set(ScreenPairing(pairingCode: pairingCode, paired: false))
         .then((value) => print("Added pairing code"))
         .catchError((error) => print("Failed to add pairing code: $error"));
   }
