5760b5ee81682db35a20454515a44bd7e2a87022
kelvin589 <190081735@aston.ac.uk>
2022-02-20 16:51:56 +0000

    Add x and y to message

--- lib/core/models/messages_model.dart
+++ lib/core/models/messages_model.dart
@@ -1,19 +1,30 @@
 class Message {
-  Message({required this.header, required this.message,});
+  Message({
+    required this.header,
+    required this.message,
+    required this.x,
+    required this.y,
+  });
 
   Message.fromJson(Map<String, Object?> json)
       : this(
           header: json['header']! as String,
           message: json['message']! as String,
+          x: json['x']! as double,
+          y: json['y']! as double,
         );
 
   final String header;
   final String message;
+  final double x;
+  final double y;
 
   Map<String, Object?> toJson() {
     return {
       'header': header,
       'message': message,
+      'x': x,
+      'y': y,
     };
   }
 }
