10038aea3672cfaacfc6e4562d93eb32941a341f
kelvin589 <190081735@aston.ac.uk>
2022-02-28 21:52:33 +0000

    Test that screen pairing info is received
    Add matcher for ScreenPairing

--- test/core/repositories/firebase_pairing_repository_test.dart
+++ test/core/repositories/firebase_pairing_repository_test.dart
@@ -1,5 +1,6 @@
 import 'dart:math';
 
+import 'package:diginotescreen/core/models/screen_pairing_model.dart';
 import 'package:diginotescreen/core/repositories/firebase_pairing_repository.dart';
 import 'package:diginotescreen/firebase_options.dart';
 import 'package:diginotescreen/ui/shared/device_info.dart';
@@ -7,6 +8,10 @@ import 'package:fake_cloud_firestore/fake_cloud_firestore.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+
+import '../../document_snapshot_matcher.dart';
+import '../../query_snapshot_matcher.dart';
+import '../../screen_pairing_matcher.dart';
 
 void main() async {
   const String token = "token";
@@ -31,12 +36,43 @@ void main() async {
     final snapshot =
         await firestoreInstance.collection("pairingCodes").doc(token).get();
     expect(snapshot.exists, true);
-    
+
     final pairingInfo = snapshot.data();
     if (pairingInfo != null) {
       expect(pairingInfo["pairingCode"], pairingCode);
       expect(pairingInfo["width"], width);
       expect(pairingInfo["height"], height);
     }
+  });
+
+  test('Screen pairing info is received', () async {
+    final ScreenPairing screenPairing = ScreenPairing(
+        pairingCode: "ABC123",
+        paired: false,
+        name: "name",
+        userID: "userID",
+        lastUpdated: DateTime.now(),
+        screenToken: token,
+        width: 100,
+        height: 100);
+
+    expect(
+      pairingRepository.getStream(),
+      emitsInOrder([
+        null,
+        ScreenPairingMatcher(screenPairing.toJson()),
+      ]),
+    );
+
+    await Future.delayed(Duration.zero);
+    await firestoreInstance
+        .collection('pairingCodes')
+        .doc(token)
+        .withConverter<ScreenPairing>(
+          fromFirestore: (snapshot, _) =>
+              ScreenPairing.fromJson(snapshot.data()!),
+          toFirestore: (screenPairing, _) => screenPairing.toJson(),
+        )
+        .set(screenPairing);
   });
 }
