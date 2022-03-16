610ade91bef8f34cc6484ae1bbaf3ac85b7bf76b
kelvin589 <190081735@aston.ac.uk>
2022-03-16 11:14:53 +0000

    Update screen model
    Add battery reporting delay

--- lib/core/models/screen_info_model.dart
+++ lib/core/models/screen_info_model.dart
@@ -1,5 +1,5 @@
 class ScreenInfo {
-  ScreenInfo({required this.screenToken, required this.batteryPercentage, required this.lowBatteryThreshold, required this.lowBatteryNotificationDelay});
+  ScreenInfo({required this.screenToken, required this.batteryPercentage, required this.lowBatteryThreshold, required this.lowBatteryNotificationDelay, required this.updateBatteryDelay});
 
   ScreenInfo.fromJson(Map<String, Object?> json)
       : this(
@@ -7,12 +7,14 @@ class ScreenInfo {
           batteryPercentage: json['batteryPercentage']! as int,
           lowBatteryThreshold: json['lowBatteryThreshold']! as int,
           lowBatteryNotificationDelay: json['lowBatteryNotificationDelay']! as int,
+          updateBatteryDelay: json['updateBatteryDelay']! as int,
         );
 
   final String screenToken;
   final int batteryPercentage;
   int lowBatteryThreshold;
   int lowBatteryNotificationDelay;
+  int updateBatteryDelay;
 
   Map<String, Object?> toJson() {
     return {
@@ -20,6 +22,7 @@ class ScreenInfo {
       'batteryPercentage': batteryPercentage,
       'lowBatteryThreshold': lowBatteryThreshold,
       'lowBatteryNotificationDelay': lowBatteryNotificationDelay,
+      'updateBatteryDelay': updateBatteryDelay,
     };
   }
 }
