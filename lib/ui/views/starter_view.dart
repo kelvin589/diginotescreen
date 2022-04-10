e90d01b87146459efe588c244241e98a0fa11df4
kelvin589 <190081735@aston.ac.uk>
2022-04-10 23:01:46 +0100

    Move MainView to a new file

--- lib/ui/views/starter_view.dart
+++ lib/ui/views/starter_view.dart
@@ -1,10 +1,8 @@
 import 'package:diginotescreen/core/models/screen_model.dart';
 import 'package:diginotescreen/core/providers/firebase_pairing_provider.dart';
-import 'package:diginotescreen/ui/shared/device_info.dart';
 import 'package:diginotescreen/ui/shared/timer_provider.dart';
+import 'package:diginotescreen/ui/views/main_view.dart';
 import 'package:diginotescreen/ui/views/preview_view.dart';
-import 'package:diginotescreen/ui/widgets/header.dart';
-import 'package:diginotescreen/ui/widgets/pairing_code_text.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
@@ -42,47 +40,6 @@ class StarterView extends StatelessWidget {
           );
         }
       },
-    );
-  }
-}
-
-class MainView extends StatelessWidget {
-  const MainView({Key? key, required this.mainContext}) : super(key: key);
-
-  final BuildContext mainContext;
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: SafeArea(
-        minimum: const EdgeInsets.all(20),
-        child: Column(
-          children: <Widget>[
-            const Header(),
-            Expanded(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: <Widget>[
-                  const Text(
-                    'Welcome, enter the following code to pair this screen:',
-                    textAlign: TextAlign.center,
-                    style: TextStyle(fontSize: 22),
-                  ),
-                  PairingCodeText(
-                    onPairingCodeGenerated: (pairingCode) async {
-                      await Provider.of<FirebasePairingProvider>(context,
-                              listen: false)
-                          .addPairingCode(
-                              pairingCode, DeviceInfo(context: context));
-                    },
-                  ),
-                ],
-              ),
-            ),
-          ],
-        ),
-      ),
     );
   }
 }
