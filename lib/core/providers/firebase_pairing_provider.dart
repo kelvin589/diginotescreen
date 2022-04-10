74c2c451c257d3b948cc240c4ae08962037e6bbd
kelvin589 <190081735@aston.ac.uk>
2022-04-10 22:29:47 +0100

    Add comments to the providers

--- lib/core/providers/firebase_pairing_provider.dart
+++ lib/core/providers/firebase_pairing_provider.dart
@@ -4,25 +4,37 @@ import 'package:diginotescreen/core/repositories/firebase_pairing_repository.dar
 import 'package:diginotescreen/ui/shared/device_info.dart';
 import 'package:flutter/material.dart';
 
+/// A provider using the [FirebasePairingRepository].
+/// 
+/// Updates the pairing code, receives a stream of [Screen], and retrieves the token.
 class FirebasePairingProvider extends ChangeNotifier {
+  /// Creates a [FirebasePairingProvider] using a [FirebaseFirestore],
+  /// for a given screen [token].
+  /// 
+  /// Initialises the [FirebasePairingRepository].
   FirebasePairingProvider(
       {required FirebaseFirestore firestoreInstance, String? token})
       : _pairingRepository = FirebasePairingRepository(
             firestoreInstance: firestoreInstance, token: token);
 
+  /// The [FirebasePairingRepository] instance.
   final FirebasePairingRepository _pairingRepository;
 
+  /// Initialises the [FirebasePairingProvider]. 
   Future<String?> init() async {
     return _pairingRepository.init();
   }
 
+  /// Updates the [pairingCode] and [deviceInfo] for this screen.
   Future<void> addPairingCode(String pairingCode, DeviceInfo deviceInfo) async {
     await _pairingRepository.addPairingCode(pairingCode, deviceInfo);
   }
 
+  /// Retrieves a stream of [Screen] information for this screen.
   Stream<Screen?> getStream() {
     return _pairingRepository.getStream();
   }
 
+  /// Retrieves the screen token.
   String? getToken() => _pairingRepository.getToken();
 }
--- lib/core/providers/firebase_preview_provider.dart
+++ lib/core/providers/firebase_preview_provider.dart
@@ -3,21 +3,32 @@ import 'package:diginotescreen/core/models/messages_model.dart';
 import 'package:diginotescreen/core/repositories/firebase_preview_repository.dart';
 import 'package:flutter/material.dart';
 
+/// A provider using the [FirebasePreviewRepository].
+///
+/// Retrieves and deletes messages, and retrieves the email of the user associated
+/// with this screen.
 class FirebasePreviewProvider extends ChangeNotifier {
+  /// Creates a [FirebasePreviewProvider] using a [FirebaseFirestore] instance.
+  ///
+  /// Initialises the [FirebasePreviewRepository].
   FirebasePreviewProvider({required FirebaseFirestore firestoreInstance})
       : _previewRepository =
             FirebasePreviewRepository(firestoreInstance: firestoreInstance);
 
+  /// The [FirebasePreviewRepository] instance.
   final FirebasePreviewRepository _previewRepository;
 
+  /// Retrieves a stream of [Message] for the given [screenToken].
   Stream<Iterable<Message>> getMessages(String screenToken) {
     return _previewRepository.getMessages(screenToken);
   }
 
+  /// Deletes the [Message] with [messageID] for the given [screenToken].
   void deleteMessage(String screenToken, String messageID) {
     _previewRepository.deleteMessage(screenToken, messageID);
   }
 
+  /// Retrieves the email of the user associated with this screen.
   Future<String> getUsersEmail(String screenToken) async {
     return await _previewRepository.getUsersEmail(screenToken);
   }
